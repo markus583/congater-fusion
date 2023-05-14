d92677e9a4334d0cf1be1483f7b13b2f6b421b02
markus583 <markus.frohmann@gmail.com>
2023-05-14 12:10:44 +0200

    clean up project

--- src/scripts/probing/run_glue.sh
+++ src/scripts/probing/run_glue.sh
@@ -23,7 +23,7 @@ else
   SEED=$NEW_SEED
 fi
 
-for TASK in qqp; do
+for TASK in mrpc rte sst2 cola stsb qnli mnli qqp; do
   for SEED in $SEED; do
     if [ $TASK = "cola" ]; then
         EVAL_METRIC="eval_matthews_correlation"
@@ -69,7 +69,7 @@ for TASK in qqp; do
           --max_seq_length 128 \
           --do_train \
           --do_eval \
-          --eval_adapter True\
+          --eval_adapter True \
           --per_device_train_batch_size 32 \
           --per_device_eval_batch_size 32 \
           --dataloader_num_workers 0 \
@@ -78,7 +78,7 @@ for TASK in qqp; do
           --max_steps 1 \
           --train_adapter \
           --adapter_config congaterV3[omega=$omega] \
-          --output_dir ../../runs/PROBE/ct_0-a-RELUcopy/$OMEGA/$TASK/$MODEL_NAME/$TRAIN_PCT/$SEED \
+          --output_dir ../../runs/PROBE/ct_0-a-RELU-PLUS-LN_BEFORE/$OMEGA/$TASK/$MODEL_NAME/$TRAIN_PCT/$SEED \
           --logging_strategy epoch \
           --save_strategy epoch \
           --evaluation_strategy epoch \
@@ -87,7 +87,7 @@ for TASK in qqp; do
           --load_best_model_at_end True \
           --metric_for_best_model $EVAL_METRIC \
           --report_to wandb \
-          --run_name $TASK-$MODEL_NAME-$TRAIN_PCT-$SEED-PROBE-V1-FIX-$OMEGA \
+          --run_name $TASK-$MODEL_NAME-$TRAIN_PCT-$SEED-PROBE-V0-$OMEGA \
           --max_train_pct $TRAIN_PCT \
           --seed $SEED \
           --omega $omega
